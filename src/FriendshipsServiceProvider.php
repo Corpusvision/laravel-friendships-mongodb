f588a58fd5dcf18c61213f98a567e8847a73d513
Pranjal Pandey <pranjaltech10@gmail.com>
2018-08-16 07:05:43 +0530

    Update FriendshipsServiceProvider.php

--- src/FriendshipsServiceProvider.php
+++ src/FriendshipsServiceProvider.php
@@ -14,18 +14,7 @@ class FriendshipsServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        if (class_exists('CreateFriendshipsTable') || class_exists('CreateFriendshipsGroupsTable')) {
-            return;
-        }
-
-        $stub      = __DIR__ . '/database/migrations/';
-        $target    = database_path('migrations') . '/';
-
-        $this->publishes([
-            $stub . 'create_friendships_table.php'        => $target . date('Y_m_d_His', time()) . '_create_friendships_table.php',
-            $stub . 'create_friendships_groups_table.php' => $target . date('Y_m_d_His', time() + 1) . '_create_friendships_groups_table.php'
-        ], 'migrations');
-
+       
         $this->publishes([
             __DIR__ . '/config/friendships.php' => config_path('friendships.php'),
         ], 'config');
